e05cab8cd6c57be5069897093b7f6828db01b92c
Jose <jgsp2@cin.ufpe.br>
2021-07-18 20:38:12 -0300

    add utils file

---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#include <vector>
+
+class Utils{
+ // TODO: aqui vao ficar metodos tipo: ler de csv, criar funcoes hash, pegar parametros (ou a gt pode separar em 3 tbm)
+ // tbm n precisa ser uma classe, a gt pode só sair metendo os metodos e importar eles ou fazer mais de uma classe nesse arquivo tb, sei la
+    private:
+
+    public:
+        std::vector<int> getNewHashFunction(int P){
+            // TODO: generate random a and b mod P
+            return {1,2};
+        }
+};
