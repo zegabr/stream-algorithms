fa7a1489cca6c8f4a889a8d5dd2241943d899e30
Jose <jgsp2@cin.ufpe.br>
2021-07-20 21:43:54 -0300

    refactor: add utilities to Uyils.cpp

--- src/cmin.cpp
+++ src/cmin.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
-#include <cmath>
-#include <sstream>
-#include <fstream>
-#include <set>
-#include <queue>
-#include <vector>
-#include <climits>
 #include "utils.h"
 
-
 using namespace std;
+
+Utils utils; // global utility functions
 
 class CountMinSketch{
     private:
         int P = INT_MAX; // mersenne prime => 2 ^31 - 1
         int t, k;
         vector<vector<int>> hashFunctions, C;
-        Utils U;
 
         /**
-         * Returns the i-th position of x
+         * Returns the i-th hashed position of x
          * */
         int getHash(int i, int x){
-            vector<int> &ab = hashFunctions[i];
-            // return ((ab[0] * x % P + b) % P) % k;
-            return 0;
+            long long a = hashFunctions[i][0];
+            long long b = hashFunctions[i][1];
+            return ((a * x % P + b) % P) % k;
         }
 
     public:
@@ -32,9 +24,10 @@ class CountMinSketch{
             k = int(2.0/eps);
             t = ceil(log(1.0/delta));
             for(int i = 0; i < t; i++){
-                hashFunctions.push_back(U.getNewHashFunction(P));
+                hashFunctions.push_back(utils.getNewHashFunction(P));
             }
-            C.assign(t, vector<int>(0,k)); // C matrix (t x k)
+            cout << endl;
+            C.assign(t, vector<int>(k,0)); // C matrix (t x k)
         }
 
         void update(int x, int w){
@@ -54,158 +47,57 @@ class CountMinSketch{
         }
 };
 
-const set<string> possibleOptions = {
-    "--id",
-    "--weight",
-    "--eps",
-    "--delta",
-    "--query",
-    "--qryfile"
-};
-
-int id = 0; // default column to be used as id
-int weight = 4; // default column to be used as weights
-double eps = 0.1; // default error bound
-double delta = 0.05;// default error probability
-vector<int> queryIds; // ids to be queried in the end
-
-queue<string> argsQueue; // queue to store args temporarely
-
-void invalidArgument(string &arg){
-    cerr << "Invalid argument: " << arg << '\n';
-    exit(1);
-}
-
-void missingArgumentValue(string &arg){
-    cerr << "missing argument value for " << arg << '\n';
-    exit(1);
-}
-
-void checkArg(string &arg){
-    if(!possibleOptions.count(arg)){
-        invalidArgument(arg);
-    }
-}
-
-void tryUpdateArg(string &arg, string argName, int &var){
-    if(arg == argName){
-        if(argsQueue.empty()){
-            missingArgumentValue(arg);
-        }
-        if(possibleOptions.count(argsQueue.front())){
-            missingArgumentValue(arg);
-        }
-        var = stoi(argsQueue.front());
-        argsQueue.pop();
-    }
-}
-
-void tryUpdateArg(string &arg, string argName, double &var){
-    if(arg == argName){
-        if(argsQueue.empty()){
-            missingArgumentValue(arg);
-        }
-        if(possibleOptions.count(argsQueue.front())){
-            missingArgumentValue(arg);
-        }
-        var = stod(argsQueue.front());
-        argsQueue.pop();
-    }
-}
-
-void tryUpdateQueryList(string &arg, string argName){
-    if(arg == argName){
-        if(argsQueue.empty()){
-            missingArgumentValue(arg);
-        }
-        queryIds.clear();
-        while(argsQueue.size() > 0 and !possibleOptions.count(argsQueue.front())){
-            queryIds.push_back(stoi(argsQueue.front()));
-            argsQueue.pop();
-        }
-        if(queryIds.empty()){
-            missingArgumentValue(arg);
-        }
-    }
-}
-
-stringstream getFileAsStream(string filename){
-    ifstream f(filename);
-    stringstream buffer;
-    buffer << f.rdbuf();
-    return buffer;
-}
-
-void tryUpdateQueryListByFile(string &arg, string argName){
-    if(arg == argName){
-        if(argsQueue.empty()){
-            missingArgumentValue(arg);
-        }
-
-        queryIds.clear();
-        if(!possibleOptions.count(argsQueue.front())){
-            string filename = argsQueue.front();
-            argsQueue.pop();
-
-            stringstream ss = getFileAsStream(filename);
-
-            int qid;
-            while(ss >> qid){
-                queryIds.push_back(qid);
-            }
-
-        }
-        if(queryIds.empty()){
-            cerr << "empty file" << '\n';
-            exit(1);
-        }
-    }
-}
-void updateArgs(){
-    while(argsQueue.size() > 0){
-        string arg = argsQueue.front();
-        argsQueue.pop();
-
-        checkArg(arg);
-        tryUpdateArg(arg, "--id", id);
-        tryUpdateArg(arg, "--weight", weight);
-        tryUpdateArg(arg, "--eps", eps);
-        tryUpdateArg(arg, "--delta", delta);
-        tryUpdateQueryList(arg, "--query");
-        tryUpdateQueryListByFile(arg, "--qryfile");
-    }
-}
-
-void printvec(vector<int> &v){
-    for(int i = 0; i < (int)v.size(); i++){
-        cout << v[i] << " \n"[i==v.size()-1];
-    }
-}
 
 int main(int args, char **argv){
-    cout << args << endl;
-    for(int i = 1; i < args - 1; i++){
+
+    set<string> possibleOptions = {
+        "--id",
+        "--weight",
+        "--eps",
+        "--delta",
+        "--query",
+        "--qryfile"
+    };
+    int id = 0; // default column to be used as id
+    int weight = 4; // default column to be used as weights
+    double eps = 0.1; // default error bound
+    double delta = 0.05;// default error probability
+    vector<int> queryIds; // ids to be queried in the end
+
+    queue<string> argsQueue; // queue to store args temporarely
+    for(int i = 1; i < args - 1; i++){ // get from second (since first is the command) to before last (since last is the csv file)
         argsQueue.push(argv[i]);
     }
 
-    string datasetFilename = argv[args - 1];
+    string datasetFilename = argv[args - 1]; // csv filename
 
-    updateArgs();
-
+    utils.updateArgsCountMin(
+            possibleOptions, 
+            argsQueue, 
+            id, 
+            weight, 
+            eps, 
+            delta, 
+            queryIds
+            );
 
     cout << "using: " << '\n';
     cout << "id=" << id << '\n';
     cout << "weight=" << weight << '\n';
     cout << "eps=" << eps << '\n';
     cout << "delta=" << delta << '\n';
-    cout << "queries=";
-    printvec(queryIds);
+    cout << "queries="; utils.printvec(queryIds);
+    cout << "dataset filename = " << datasetFilename << '\n';
 
+    utils.loadFileAsStream(datasetFilename, id, weight);
     CountMinSketch sketch(eps, delta);
+
 
     // TODO: ler x e w do dataset seguindo as informacoes nos parametros
 
     for(int q : queryIds){
+
+        cout << "pegando um" << endl;
         cout << sketch.query(q) << ' ';
     }
     cout << '\n';
--- src/utils.h
+++ src/utils.h
@@ -1,15 +1,183 @@
 #ifndef UTILS_TAA
 #define UTILS_TAA 1
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <fstream>
+#include <set>
+#include <queue>
 #include <vector>
+#include <climits>
+
+using namespace std;
 
 class Utils{
- // TODO: aqui vao ficar metodos tipo: ler de csv, criar funcoes hash, pegar parametros (ou a gt pode separar em 3 tbm)
+    // TODO: separar isso em mais arquivos?
     private:
 
-    public:
-        std::vector<int> getNewHashFunction(int P){
-            // TODO: generate random a and b mod P
-            return {1,2};
+    public: // Constructor, initialize global variables here
+        Utils(){
+            reset_seed();
+        }
+
+    public: // random generator and hash functions generator
+        void reset_seed(){
+            srand(unsigned(time(NULL)));
+        }
+
+        int randint(int l, int r){//random from l to r inclusive
+            return rand() % (r-l+1) + l;   
+        }
+
+        /**
+         * Returns a pair (a,b) to be using in the hash function h(x) = ax+b
+         */
+        vector<int> getNewHashFunction(int P){
+            int a = randint(0, P-1); // TODO: trocar isso pra randint(1, P-1)? pra que h(x) = ax+b nao degenere pra h(x) = b ??
+            int b = randint(0, P-1);
+            return {a,b};
+        }
+
+    public: // args processor
+        void updateArgsCountMin(
+                set<string> &possibleOptions, 
+                queue<string> &argsQueue, 
+                int &id, 
+                int &weight, 
+                double &eps, 
+                double &delta, 
+                vector<int> &queryIds
+                )
+        {
+            while(argsQueue.size() > 0){
+                string arg = argsQueue.front();
+                argsQueue.pop();
+
+                checkArg(possibleOptions, arg);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--id", id);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--weight", weight);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--eps", eps);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--delta", delta);
+                tryUpdateQueryList(possibleOptions, argsQueue, arg, "--query", queryIds);
+                tryUpdateQueryListByFile(possibleOptions, argsQueue, arg, "--qryfile", queryIds);
+            }
+        }
+
+        void updateArgsKMV(// TODO: @gabi usar soh usar isso aqui do jeito q eu usei o de cima :eyes: (dps apaga esse comment)
+                set<string> &possibleOptions, 
+                queue<string> &argsQueue, 
+                int &target, 
+                double &eps, 
+                double &delta 
+                )
+        {
+            while(argsQueue.size() > 0){
+                string arg = argsQueue.front();
+                argsQueue.pop();
+
+                checkArg(possibleOptions, arg);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--target", target);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--eps", eps);
+                tryUpdateArg(possibleOptions, argsQueue, arg, "--delta", delta);
+            }
+
+        }
+
+        void invalidArgument(string &arg){
+            cerr << "Invalid argument: " << arg << '\n';
+            exit(1);
+        }
+
+        void missingArgumentValue(string &arg){
+            cerr << "missing argument value for " << arg << '\n';
+            exit(1);
+        }
+
+        void checkArg(set<string> &possibleOptions, string &arg){
+            if(!possibleOptions.count(arg)){
+                invalidArgument(arg);
+            }
+        }
+
+        void tryUpdateArg(set<string> &possibleOptions, queue<string> &argsQueue, string &arg, string argName, int &var){
+            if(arg == argName){
+                if(argsQueue.empty() or possibleOptions.count(argsQueue.front())){
+                    missingArgumentValue(arg);
+                }
+                var = stoi(argsQueue.front());
+                argsQueue.pop();
+            }
+        }
+
+        void tryUpdateArg(set<string> &possibleOptions, queue<string> &argsQueue, string &arg, string argName, double &var){
+            if(arg == argName){
+                if(argsQueue.empty() or possibleOptions.count(argsQueue.front())){
+                    missingArgumentValue(arg);
+                }
+                var = stod(argsQueue.front());
+                argsQueue.pop();
+            }
+        }
+
+        void tryUpdateQueryList(set<string> &possibleOptions, queue<string> &argsQueue,string &arg, string argName, vector<int> &queryIds){
+            if(arg == argName){
+                if(argsQueue.empty()){
+                    missingArgumentValue(arg);
+                }
+                queryIds.clear();
+                while(argsQueue.size() > 0 and !possibleOptions.count(argsQueue.front())){
+                    queryIds.push_back(stoi(argsQueue.front()));
+                    argsQueue.pop();
+                }
+                if(queryIds.empty()){
+                    missingArgumentValue(arg);
+                }
+            }
+        }
+
+        void tryUpdateQueryListByFile(set<string> &possibleOptions, queue<string> &argsQueue,string &arg, string argName, vector<int> &queryIds){
+            if(arg == argName){
+                if(argsQueue.empty()){
+                    missingArgumentValue(arg);
+                }
+
+                queryIds.clear();
+                if(!possibleOptions.count(argsQueue.front())){
+                    string filename = argsQueue.front();
+                    argsQueue.pop();
+
+                    stringstream ss = getFileAsStream(filename);
+
+                    int qid;
+                    while(ss >> qid){
+                        queryIds.push_back(qid);
+                    }
+
+                }
+                if(queryIds.empty()){
+                    cerr << "empty file" << '\n';
+                    exit(1);
+                }
+            }
+        }
+
+        stringstream getFileAsStream(string &filename){
+            ifstream f(filename);
+            stringstream buffer;
+            buffer << f.rdbuf();
+            return buffer;
+        }
+
+    public: // csv utilities
+    void loadFileAsStream(string &filename, int columnId, int weightColumnId){
+        // TODO: carregar isso num campo global (private) do Utils, pra que consiga chamar algo tipo utils.getNextEntry
+    }
+
+    public: // debug utilities
+        void printvec(vector<int> &v){
+            for(int i = 0; i < (int)v.size(); i++){
+                cout << v[i] << " \n"[i==v.size()-1];
+            }
         }
 };
 
